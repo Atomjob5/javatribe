e91a9117a2aed76341195e057df6def5b4f61def
ry-yuan <1094324110@qq.com>
2018-04-02 00:12:14 +0800

    add:增加此分支，存放设计稿

--- src/main/java/com/javatribe/dao/HomeCustomMapper.java
+++
@@ -1,9 +0,0 @@
-package com.javatribe.dao;
-
-import java.util.List;
-
-import com.javatribe.pojo.HomeCustom;
-
-public interface HomeCustomMapper {
-	List<HomeCustom> selectHomeInfo() throws Exception;
-}
--- src/main/java/com/javatribe/pojo/ProjectCustom.java
+++
@@ -1,6 +0,0 @@
-package com.javatribe.pojo;
-
-public class ProjectCustom extends Project{
-	//添加项目信息的扩展属性
-
-}
