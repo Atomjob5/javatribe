606757e18a5290dc7b1c5524ce5edd08e14e318d
sanjun <593721042@qq.com>
2018-04-13 11:47:09 +0800

    delete javatribe-master

--- javatribe-master/src/main/java/com/javatribe/dao/AboutCustomMapper.java
+++
@@ -1,9 +0,0 @@
-package com.javatribe.dao;
-
-import java.util.List;
-
-import com.javatribe.pojo.About;
-
-public interface AboutCustomMapper {
-   List<About> selectAll() ;
-}
--- javatribe-master/src/main/java/com/javatribe/pojo/HomeCustom.java
+++
@@ -1,5 +0,0 @@
-package com.javatribe.pojo;
-//多系统生成的Home进行封装
-public class HomeCustom extends Home {
-
-}
